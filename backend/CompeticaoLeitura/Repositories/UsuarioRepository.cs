c5eba6dd21973afc6277ed4e5aa095a263957a6a
hiraeth098 <breninhomc098@gmail.com>
2026-03-27 17:54:46 -0300

    minnor fixes

---
+++ backend/CompeticaoLeitura/Repositories/UsuarioRepository.cs
@@ -0,0 +1,10 @@
+namespace CompeticaoLeitura.Repositories;
+
+using Dapper;
+using Microsoft.Data.Sqlite;
+using CompeticaoLeitura.Models;
+
+public class UsuarioRepository
+{
+    public UsuarioRepository
+}
